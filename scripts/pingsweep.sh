515c32279fb06f666f3708c30511b0b287076711
lukkelele <encryptedluke@protonmail.com>
2021-11-20 13:15:49 +0100

    pingsweep.sh added

---
+++ scripts/pingsweep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+figlet pingsweep
+echo " 
+
+"
+
+if [ "$1" == "" ]
+then
+echo "
+	usage: ./pingsweep.sh [network-addr]
+      example: ./pingsweep.sh 192.168.52
+
+      						"
+else
+	# Iterate 1-254
+	# Grab successful ping and cut with " " as delimiter
+	# Fetch the fourth field
+	# Delete the final character from each line using sed
+echo "
+Sweeping....
+"
+for x in {1..254}
+do
+	ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//' 
+done
+fi
