d3a5939afb94c052cabe836cca9f8bf6856e5428
lukkelele <encryptedluke@protonmail.com>
2022-05-04 22:40:17 +0200

    gn

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 WHITE='\033[1;37m'
+BROWN='\033[0;33m'
 BLACK='\033[0;30m'
 PURPLE='\033[0;35m'
 YELLOW='\033[1;33m'
-LIGHTCYAN='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTGRAY='\033[0;37m'
+LIGHTCYAN='\033[1;36m'
+LIGHTCYAN='\033[1;36m'
 LIGHTCYAN='\033[1;30m'
 LIGHTRED='\033[1;31m'
 LIGHTGREEN='\033[1;32m'
 NC='\033[0m'
 
-if [ "$1" == "YELLOW" ]
-then
-  COLOR=$YELLOW
-elif [ "$1" == "GREEN" ]
-then
-  COLOR=$GREEN
-elif [ "$1" == "BLACK" ]
-then
-  COLOR=$BLACK
-elif [ "$1" == "WHITE" ]
-then
-  COLOR=$WHITE
-elif [ "$1" == "CYAN" ]
-then
-  COLOR=$CYAN
-elif [ "$1" == "PURPLE" ]
-then
-  COLOR=$PURPLE
-elif [ "$1" == "RED" ]
-then
-  COLOR=$RED
-fi
 
-printf "==>${COLOR} $2 ${NC}\n"
+function print_color () {
+  case $1 in
+    red) COLOR=$RED ;;
+    blue) COLOR=$BLUE ;;
+    green) COLOR=$GREEN ;;
+    cyan) COLOR=$CYAN ;;
+    white) COLOR=$WHITE ;;
+    purple) COLOR=$WHITE ;;
+    yellow) COLOR=$YELLOW ;;
+    brown) COLOR=$BROWN ;;
+    black) COLOR=$BLACK ;;
+    darkgray) COLOR=$DARKGRAY ;;
+    lightgray) COLOR=$LIGHTGRAY ;;
+    lightgreen) COLOR=$LIGHTGREEN ;;
+    lightcyan ) COLOR=$LIGHTCYAN ;;
 
+  esac
+
+  printf "==>${COLOR} $2 ${NC}\n"
+}
+
+function arrow () {
+  case $3 in
+    red) COLOR=$RED ;;
+    blue) COLOR=$BLUE ;;
+    green) COLOR=$GREEN ;;
+    cyan) COLOR=$CYAN ;;
+    white) COLOR=$WHITE ;;
+    purple) COLOR=$WHITE ;;
+    yellow) COLOR=$YELLOW ;;
+    brown) COLOR=$BROWN ;;
+    black) COLOR=$BLACK ;;
+    darkgray) COLOR=$DARKGRAY ;;
+    lightgray) COLOR=$LIGHTGRAY ;;
+    lightgreen) COLOR=$LIGHTGREEN ;;
+    lightcyan ) COLOR=$LIGHTCYAN ;;
+  esac
+  printf "${COLOR}==>${NC} $2 ${NC}"
+}
+arrow $1 $2 $3
+#print_color $1 "$2"
+echo ''
