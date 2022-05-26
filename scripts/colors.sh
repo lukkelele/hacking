d5ccc8078326aaded65e9d0e01c8cf7d01319f79
lukkelele <encryptedluke@protonmail.com>
2022-05-26 09:03:17 +0200

    colors.sh script modified

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BUFFER_SIZE=55
+
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
@@ -18,15 +20,14 @@ LIGHTRED='\033[1;31m'
 LIGHTGREEN='\033[1;32m'
 NC='\033[0m'
 
-
-function print_color () {
+function get_color () {
   case $1 in
     red) COLOR=$RED ;;
     blue) COLOR=$BLUE ;;
     green) COLOR=$GREEN ;;
     cyan) COLOR=$CYAN ;;
     white) COLOR=$WHITE ;;
-    purple) COLOR=$WHITE ;;
+    purple) COLOR=$PURPLE ;;
     yellow) COLOR=$YELLOW ;;
     brown) COLOR=$BROWN ;;
     black) COLOR=$BLACK ;;
@@ -34,30 +35,29 @@ function print_color () {
     lightgray) COLOR=$LIGHTGRAY ;;
     lightgreen) COLOR=$LIGHTGREEN ;;
     lightcyan ) COLOR=$LIGHTCYAN ;;
-
   esac
-
-  printf "==>${COLOR} $2 ${NC}\n"
 }
 
+function print_color () {
+  get_color $1
+  printf "${COLOR} $2 ${NC}\n"
+}
+
+# Colored arrow, ==> , without a linebreak
 function arrow () {
-  case $3 in
-    red) COLOR=$RED ;;
-    blue) COLOR=$BLUE ;;
-    green) COLOR=$GREEN ;;
-    cyan) COLOR=$CYAN ;;
-    white) COLOR=$WHITE ;;
-    purple) COLOR=$WHITE ;;
-    yellow) COLOR=$YELLOW ;;
-    brown) COLOR=$BROWN ;;
-    black) COLOR=$BLACK ;;
-    darkgray) COLOR=$DARKGRAY ;;
-    lightgray) COLOR=$LIGHTGRAY ;;
-    lightgreen) COLOR=$LIGHTGREEN ;;
-    lightcyan ) COLOR=$LIGHTCYAN ;;
-  esac
-  printf "${COLOR}==>${NC} $2 ${NC}"
+  get_color $1
+  printf "${COLOR}==>${NC}"
 }
-arrow $1 $2 $3
-#print_color $1 "$2"
-echo ''
+
+function arrow_colortext () {
+  for i in "${@:3:$BUFFER_SIZE}"
+  do
+    if [[ $i =~ : ]]; then :
+    else str+=$i" "
+    fi
+  done
+  arrow $1
+  print_color $2 "${str}"
+}
+
+arrow_colortext $1 $2 "${@:3:$BUFFER_SIZE}"
